3d72c01935a2e26b90f8f10a42f61150733ebf74
ArthurJuwer <arthur.juwer99@gmail.com>
2025-03-16 12:23:31 -0300

    correção na logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ import LogoGess from "../../public/Logo-Gess.svg";
 export default function Menu() {
   return (
     <header className="flex justify-between items-center p-8 border-b-4 border-gray-400">
-        <Image src={LogoGess} alt="Logo Do GESS" className="w-44"></Image>
+        <Image src={LogoGess} alt="Logo Do GESS" className="w-40 mt-1"></Image>
         <div className="flex flex-col gap-2">
             <span className="w-9 h-1 bg-[#FA7B2B] block rounded"></span>
             <span className="w-9 h-1 bg-[#FA7B2B] block rounded"></span>
